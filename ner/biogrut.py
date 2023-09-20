de50664561ae8d572d77adc0db40fe97681d6726
Billhappi <billhappi@gmail.com>
2023-09-20 10:59:49 +0200

    WIP

--- ner/biogrut.py
+++ ner/biogrut.py
@@ -9,16 +9,16 @@ from module import build_model, EarlyStopping, cal_f1score, DataLoader, load_emb
 tests = ['NCBI-disease', 'BC5CDR-disease', 'BC5CDR-chem','BC4CHEMD',  'BC2GM', 'JNLPBA', 'linnaeus', 's800']
 if __name__ == '__main__':
     arg.num_epochs = 2
-    dataset = 'LLM'
-    _in = 'Dataset ' + dataset + ' is running ... 0%'
-    print(_in)
-    add_line(file_name='../result/logs/logs.txt', lines=[_in])
-    arg.choosen_dataset = dataset
-    DataProcessingMaster(dataset=dataset)
-    train.run(arg)
-    out = 'Dataset ' + dataset + ' has finished 100% !'
-    print(out)
-    add_line(file_name='../result/logs/logs.txt', lines=[out])
+    # dataset = 'LLM'
+    # _in = 'Dataset ' + dataset + ' is running ... 0%'
+    # print(_in)
+    # add_line(file_name='../result/logs/logs.txt', lines=[_in])
+    # arg.choosen_dataset = dataset
+    # DataProcessingMaster(dataset=dataset)
+    # train.run(arg)
+    # out = 'Dataset ' + dataset + ' has finished 100% !'
+    # print(out)
+    # add_line(file_name='../result/logs/logs.txt', lines=[out])
     # read file order
     labels = read_labels()
     for i in range(len(tests)) :
